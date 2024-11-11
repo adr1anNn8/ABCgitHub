b8b71ca687dc4b0c2e92c384e4f8b046e54e0b6b
adr1anNn8 <adrianzmuncila16@gmail.com>
2024-11-11 20:39:10 +0200

    Java program for SUMA

---
+++ Sumaf.java
@@ -0,0 +1,22 @@
+public class Sumaf{
+
+    private int a,b,c,d;
+    public int suma(int a,int b){
+
+        return a+b;
+    }
+    public int suma(int a , int b, int c){
+        return suma(a,b)+c;
+
+    }
+    public int suma(int a , int b, int c, ind d){
+
+        return suma(a,b,c)+d;
+    }
+    public static void main(String argv[]){
+        
+        System.out.println(suma2,3,4,5);
+
+
+    }
+    }
